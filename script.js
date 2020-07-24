b49e76106ceaadf8915278090c4c91a18c73ca5d
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-24 14:26:32 +0900

    no message

--- script.js
+++ script.js
@@ -298,7 +298,7 @@ function init() {
       marker.setPosition({ lat, lng });
       map.panTo({ lat, lng });
       const xhr = new XMLHttpRequest()
-      xhr.open('POST', 'https://cybertagapi.herokuapp.com:5000/addLocation')
+      xhr.open('POST', 'https://cybertagapi.herokuapp.com:27349/addLocation')
       xhr.send({"lat":lat,"lng":lng});
       $info.textContent = `Lat: ${lat.toFixed(5)} Lng: ${lng.toFixed(5)}`;
       $info.classList.remove('error');
