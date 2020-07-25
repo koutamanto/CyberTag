3db74a887445b4fdd016befc0c5ccfd3e411cdf2
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-25 17:02:44 +0900

    no message

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def sendLocation():
 	print(lng)
 	return datas
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
