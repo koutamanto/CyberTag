6327ed402c7c0ea19105ac7f245b3b742578ce74
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-25 17:33:21 +0900

    no message

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import json, os
+
 app = Flask(__name__)
 @app.route("/Nige")
 def Nige():
@@ -27,5 +28,8 @@ def sendLocation():
 	print(lng)
 	with open('loc.json','w') as f:
 		json.dump(datas, f, indent=4)
+server = Server(app.wsgi_app)
+# server.watch
+server.serve()
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
