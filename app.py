e4fe2fe5f16d2b1fbb372b84eb93d9e9d16447c9
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-25 17:42:35 +0900

    no message

--- app.py
+++ app.py
@@ -28,8 +28,5 @@ def sendLocation():
 	print(lng)
 	with open('loc.json','w') as f:
 		json.dump(datas, f, indent=4)
-server = Server(app.wsgi_app)
-# server.watch
-server.serve()
 if __name__ == '__main__':
-	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+	app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
