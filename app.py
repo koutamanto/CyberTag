261706a781bf0e0255776ac024285b15faa9b678
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-25 16:37:47 +0900

    no message

--- app.py
+++ app.py
@@ -3,27 +3,27 @@ import json, os
 app = Flask(__name__)
 @app.route("/Nige")
 def Nige():
-    return render_template("Nige/index.html")
+	return render_template("Nige/index.html")
 
 @app.route("/Oni")
 def Oni():
-    return render_template("Oni/index.html", datas=datas)
+	return render_template("Oni/index.html", datas=datas)
 @app.route("/getLocation", methods=["GET"])
 def getLocation():
-    return render_template("Oni/index.html",datas=datas)
+	return render_template("Oni/index.html",datas=datas)
 
 @app.route("/sendLocation", methods=["POST"])
 def sendLocation():
-    global datas
-    datas = request.get_data()
-    datas = datas.decode()
-    datas = json.loads(datas)
-    print(datas)
-    lat = datas["lat"]
-    lng = datas["lng"]
-    print(lat)
-    print(lng)
-    return datas
+	global datas
+	datas = request.get_data()
+	datas = datas.decode()
+	datas = json.loads(datas)
+	print(datas)
+	lat = datas["lat"]
+	lng = datas["lng"]
+	print(lat)
+	print(lng)
+	return datas
 if __name__ == '__main__':
-    app.listen(5000)
+	app.listen(process.env["PORT"] || 5000)
     app.run(host="0.0.0.0", port=5000)
