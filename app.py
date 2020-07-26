9955c80e8f913d0b7d052e3353d62f84f8a8f160
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-26 17:30:41 +0900

    no message

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def sendLocation():
 	print(lat)
 	print(lng)
 	with open('loc.json','w') as f:
-		json.dump(f)
+		json.dump(datas, f, indent=4)
 	return jsonify(datas)
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
