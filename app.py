709c118111624e39d4ae525f55849ccd136083ad
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-26 19:10:20 +0900

    no message

--- app.py
+++ app.py
@@ -5,30 +5,32 @@ app = Flask(__name__)
 CORS(app)
 @app.route("/Nige")
 def Nige():
-	return render_template("Nige/index.html")
+    return render_template("Nige/index.html")
 @app.route("/Oni")
 def Oni():
-	with open('loc.json','r') as f:
-		dt = json.load(f)
-	return render_template("Oni/index.html", datas=dt)
+with open('loc.json','r') as f:
+    json_txt = f.read()
+    json_txt = str(json_txt).replace("'", '"').replace('True', 'true').replace('False', 'false')
+    dt = json.loads(json_txt)
+    return render_template("Oni/index.html", datas=dt)
 @app.route("/getLocation", methods=["GET"])
 def getLocation():
-	with open('loc.json','r') as f:
-		dt = json.load(f)
-	return render_template("Oni/index.html",datas=dt)
+    with open('loc.json','r') as f:
+        dt = json.load(f)
+    return render_template("Oni/index.html",datas=dt)
 @app.route("/sendLocation", methods=["POST"])
 def sendLocation():
-	global datas
-	datas = request.get_data()
-	datas = datas.decode()
-	datas = json.loads(datas)
-	print(datas)
-	lat = datas["lat"]
-	lng = datas["lng"]
-	print(lat)
-	print(lng)
-	with open('loc.json','w') as f:
-		json.dump(datas, f, indent=4)
-	return jsonify(datas)
+    global datas
+    datas = request.get_data()
+    datas = datas.decode()
+    datas = json.loads(datas)
+    print(datas)
+    lat = datas["lat"]
+    lng = datas["lng"]
+    print(lat)
+    print(lng)
+    with open('loc.json','w') as f:
+        json.dump(datas, f, indent=4)
+    return jsonify(datas)
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
