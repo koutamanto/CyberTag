5d8a2e39b129cb49b93349d7e9aeebebf607c5e5
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-08-05 19:12:37 +0900

    no message

--- static/js/script.js
+++ static/js/script.js
@@ -293,7 +293,7 @@ function init() {
     console.log(cookies)
     for(c of cookies){
       cArray = c.split('=');
-      if(cArray[0] == ' RoomID'){
+      if(cArray[0] == 'RoomID'){
         RoomID = cArray[1]
       }
       if(cArray[0] == " name"){
